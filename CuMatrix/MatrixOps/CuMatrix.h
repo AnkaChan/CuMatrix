de6371069b4c602713f38da4f40ecf0f6b06394c
AnkaChan <ankachan92@gmail.com>
2023-01-09 10:32:48 -0700

    Changed to template function
    Signed-off-by: AnkaChan <ankachan92@gmail.com>

--- CuMatrix/MatrixOps/CuMatrix.h
+++ CuMatrix/MatrixOps/CuMatrix.h
@@ -9,67 +9,79 @@ namespace CuMatrix
 	Assuming all the matrix to be column major; 
 */
 
-	GPU_CPU_INLINE_FUNC float* vecPtr(float* buffer, int vecPos, int stride) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType* vecPtr(DType* buffer, int vecPos, int stride) {
 		return buffer + vecPos * stride;
 	}
 
 
-	GPU_CPU_INLINE_FUNC float vec2CrossProduct(float* v1, float* v2) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType vec2CrossProduct(DType* v1, DType* v2) {
 		return v1[0] * v2[1] - v1[1] * v2[0];
 
 	}
 
-	GPU_CPU_INLINE_FUNC void vec3Add(float* v1, float* v2, float* result) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC void vec3Add(DType* v1, DType* v2, DType* result) {
 		result[0] = v1[0] + v2[0];
 		result[1] = v1[1] + v2[1];
 		result[2] = v1[2] + v2[2];
 	}
 
-	GPU_CPU_INLINE_FUNC void vec3Minus(float* v1, float* v2, float* result) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC void vec3Minus(DType* v1, DType* v2, DType* result) {
 		result[0] = v1[0] - v2[0];
 		result[1] = v1[1] - v2[1];
 		result[2] = v1[2] - v2[2];
 	}
 
-	GPU_CPU_INLINE_FUNC void vec3Mul(float* v1, float a, float* result) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC void vec3Mul(DType* v1, DType a, DType* result) {
 		result[0] = v1[0] * a;
 		result[1] = v1[1] * a;
 		result[2] = v1[2] * a;
 	}
 
-	GPU_CPU_INLINE_FUNC float vec3DotProduct(float* v1, float* v2) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType vec3DotProduct(DType* v1, DType* v2) {
 		return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 	}
 
-	GPU_CPU_INLINE_FUNC void vec3CrossProduct(float* v1, float* v2, float* result) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC void vec3CrossProduct(DType* v1, DType* v2, DType* result) {
 		result[0] = v1[1] * v2[2] - v1[2] * v2[1];
 		result[1] = v1[2] * v2[0] - v1[0] * v2[2];
 		result[2] = v1[0] * v2[1] - v1[1] * v2[0];
 
 	}
 
-	GPU_CPU_INLINE_FUNC float vec3Norm(float* v) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType vec3Norm(DType* v) {
 		return sqrtf(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 	}
 
 
-	GPU_CPU_INLINE_FUNC float vec3NormSquare(float* v) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType vec3NormSquare(DType* v) {
 		return (v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 	}
 
-	GPU_CPU_INLINE_FUNC float vec3TripleProduct(float* v1, float* v2, float* v3) {
-		float crossProduct[3];
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType vec3TripleProduct(DType* v1, DType* v2, DType* v3) {
+		DType crossProduct[3];
 		// AB* (AC ^ AD);
 		vec3CrossProduct(v2, v3, crossProduct);
 
 		return vec3DotProduct(v1, crossProduct);
 	}
 
-	GPU_CPU_INLINE_FUNC float mat3IJ(float* m, int32_t row, int32_t col) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType mat3IJ(DType* m, int32_t row, int32_t col) {
 		return m[(3 * col) + row];
 	}
 
-	GPU_CPU_INLINE_FUNC void mat3VecProduct(float* m, float* v, float* result) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC void mat3VecProduct(DType* m, DType* v, DType* result) {
 		result[0] = m[0] * v[0];
 		result[1] = m[1] * v[0];
 		result[2] = m[2] * v[0];
@@ -84,24 +96,26 @@ namespace CuMatrix
 
 	}
 
-	GPU_CPU_INLINE_FUNC void mat3MatProduct(float* inA, float* inB, float* outC) {
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC void mat3MatProduct(DType* inA, DType* inB, DType* outC) {
 		mat3VecProduct(inA, inB, outC);
 		mat3VecProduct(inA, inB+3, outC+3);
 		mat3VecProduct(inA, inB+6, outC+6);
 	}
 
-	GPU_CPU_INLINE_FUNC float mat3Determinant(float* m) {
-		float  a11 = m[0]; float  a12 = m[3]; float  a13 = m[6];
-		float  a21 = m[1]; float  a22 = m[4]; float  a23 = m[7];
-		float  a31 = m[2]; float  a32 = m[5]; float  a33 = m[8];
+	template <typename DType>
+	GPU_CPU_INLINE_FUNC DType mat3Determinant(DType* m) {
+		DType  a11 = m[0]; DType  a12 = m[3]; DType  a13 = m[6];
+		DType  a21 = m[1]; DType  a22 = m[4]; DType  a23 = m[7];
+		DType  a31 = m[2]; DType  a32 = m[5]; DType  a33 = m[8];
 		return a11 * a22 * a33 + a12 * a23 * a31 + a13 * a21 * a32 - a13 * a22 * a31 - a12 * a21 * a33 - a11 * a23 * a32;
 	}
 
 
 };
 
-template <class Func>
-__global__ void parallel_for_3x3_matOps(float* matsFlatten, int numMats, Func func) {
+template <class Func, typename DType>
+__global__ void parallel_for_3x3_matOps(DType* matsFlatten, int numMats, Func func) {
 	for (int i = blockIdx.x * blockDim.x + threadIdx.x;
 		i < numMats;
 		i += blockDim.x * gridDim.x)
@@ -111,7 +125,7 @@ __global__ void parallel_for_3x3_matOps(float* matsFlatten, int numMats, Func fu
 }
 
 // multiplying 2 mat with abitary dimensions
-template <class DType>
+template <typename DType>
 __global__ void multiplicateMatrixOnDevice(DType* array_A, DType* array_B, DType* array_C, int M_p, int K_p, int N_p)
 {
 	int ix = threadIdx.x + blockDim.x * blockIdx.x;//row number
@@ -119,7 +133,7 @@ __global__ void multiplicateMatrixOnDevice(DType* array_A, DType* array_B, DType
 
 	if (ix < N_p && iy < M_p)
 	{
-		float sum = 0;
+		DType sum = 0;
 		for (int k = 0; k < K_p; k++)
 		{
 			sum += array_A[iy * K_p + k] * array_B[k * N_p + ix];
